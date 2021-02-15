7f984ba30916e2ac6a25126acff30ad701475c78
ericlumsden <elumsden@uw.edu>
2021-02-15 13:12:09 -0800

    initializing

---
+++ main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+# plotviz.py
+
+import wx
+from plotPanel import plotPanel
+
+APP_EXIT = 1
+
+class Example(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(Example,self).__init__(*args, **kwargs)
+
+        panel = wx.SplitterWindow(self)
+        cpanel = plotPanel(panel)
+        cpanel.draw()
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        menubar = wx.MenuBar()
+        viewMenu = wx.Menu()
+
+        self.shst = viewMenu.Append(wx.ID_ANY, 'Show statusbar', 'Show Statusbar', kind=wx.ITEM_CHECK)
+
+        viewMenu.Check(self.shst.GetId(), True)
+        self.Bind(wx.EVT_MENU, self.ToggleStatusBar, self.shst)
+
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText('Ready')
+
+        fileMenu = wx.Menu()
+        fileMenu.Append(wx.ID_NEW, '&New')
+        fileMenu.Append(wx.ID_OPEN, '&Open')
+        fileMenu.Append(wx.ID_SAVE, '&Save')
+        fileMenu.AppendSeparator()
+
+        imp = wx.Menu()
+        imp.Append(wx.ID_ANY, 'Import new .abf file...')
+        imp.Append(wx.ID_ANY, 'Next...') 
+        imp.Append(wx.ID_ANY, 'Previous...')
+
+        fileMenu.AppendSubMenu(imp, 'I&mport')
+
+        qmi = wx.MenuItem(fileMenu, APP_EXIT, '&Quit\tCtrl+Q')
+        fileMenu.Append(qmi)
+
+        self.Bind(wx.EVT_MENU, self.OnQuit, id=APP_EXIT)
+
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+
+        self.SetSize((950, 950))
+        self.Centre()
+        self.SetTitle('PyABF Viz')
+
+        menubar.Append(viewMenu, '&View')
+        self.SetMenuBar(menubar)
+
+   
+    def OnQuit(self, e):
+        self.Close()
+    
+    def ToggleStatusBar(self, e):
+
+        if self.shst.IsChecked():
+            self.statusbar.Show()
+        else:
+            self.statusbar.Hide()
+    
+    def ToggleToolBar(self, e):
+        if self.shtl.IsChecked():
+            self.toolbar.Show()
+        else:
+            self.toolbar.Hide()
+
+def main():
+
+    app = wx.App()
+    ex = Example(None, title='Sizing')
+    ex.Show()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
---
+++ plotPanel.py
@@ -0,0 +1,25 @@
+import matplotlib
+import numpy as np
+import pyabf
+import wx
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+matplotlib.use('WXAgg')
+
+class plotPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.figure = Figure()
+        self.axes = self.figure.add_subplot(111)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+    def draw(self):
+        t = np.arange(0.0, 3.0, 0.01)
+        s = t**2
+        self.axes.plot(t,s)
+
